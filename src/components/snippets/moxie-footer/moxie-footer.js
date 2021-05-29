be4498d372f7e4998ea015af175cf65e3583b060
Ali Raza <razaminhas11@gmail.com>
2021-05-30 02:25:13 +0500

    fixes

---
+++ src/components/snippets/moxie-footer/moxie-footer.js
@@ -0,0 +1,13 @@
+import './moxie-footer.scss';
+
+import $ from 'jquery-slim-webpack';
+
+// Testing arrow functions
+const MoxieFooter = () => {
+
+console.log('test footer :>> ');
+} ;
+MoxieFooter();
+  
+  
+ 
